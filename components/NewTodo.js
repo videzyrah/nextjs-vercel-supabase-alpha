e8dbd46f2e3546eca305afff8b0e2ce5037dad92
Greg Stull <videzyrah@gmail.com>
2022-07-03 11:29:44 -0500

    eslint displayname off

--- components/NewTodo.js
+++ components/NewTodo.js
@@ -4,6 +4,7 @@ import supabase from '../utils/supabase'
 export default ({ reload }) => {
   const [title, setTitle] = useState('')
 
+  // eslint-disable-next-line react/display-name
   const addTodo = async (e) => {
     e.preventDefault()
     await supabase.from('todos').insert({ title })
